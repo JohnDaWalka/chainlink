7831151b1f1994d18f7e7c0cfc81af4a6a61d84f
Dimitris <dimitris.grigoriou@smartcontract.com>
2024-11-13 13:26:04 +0200

    Add AttemptBuilder close

--- core/chains/evm/txm/orchestrator.go
+++ core/chains/evm/txm/orchestrator.go
@@ -111,6 +111,9 @@ func (o *Orchestrator[BLOCK_HASH, HEAD]) Close() (merr error) {
 				merr = errors.Join(merr, fmt.Errorf("Orchestrator failed to stop ForwarderManager: %w", err))
 			}
 		}
+		if err := o.attemptBuilder.Close(); err != nil {
+			merr = errors.Join(merr, fmt.Errorf("Orchestrator failed to stop AttemptBuilder: %w", err))
+		}
 		if err := o.txm.Close(); err != nil {
 			merr = errors.Join(merr, fmt.Errorf("Orchestrator failed to stop Txm: %w", err))
 		}
