6ba8f4a08073e71c24014c714611ea7811c37535
Balamurali Gopalswami <balamurali.gopalswami@smartcontract.com>
2024-11-22 11:02:49 -0500

    CCIP-4303:Enabling in-memory test in integration-tests workflow

--- integration-tests/smoke/ccip/ccip_test.go
+++ integration-tests/smoke/ccip/ccip_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink-testing-framework/lib/utils/testcontext"
-
 	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset"
 	"github.com/smartcontractkit/chainlink/integration-tests/testsetups"
 	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/onramp"
@@ -18,8 +17,7 @@ import (
 
 func TestInitialDeployOnLocal(t *testing.T) {
 	t.Parallel()
-	lggr := logger.TestLogger(t)
-	tenv, _, _ := testsetups.NewLocalDevEnvironmentWithDefaultPrice(t, lggr, nil)
+	tenv := changeset.NewMemoryEnvironmentWithJobsAndContracts(t, logger.TestLogger(t), 2, 4, nil)
 	e := tenv.Env
 	state, err := changeset.LoadOnchainState(e)
 	require.NoError(t, err)
