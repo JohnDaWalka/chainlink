b0957f2c7d3621c23f71b38d4c21399a097cb220
yashnevatia <yashvardhan.nevatia@smartcontract.com>
2024-12-04 16:56:40 +0530

    setup zksolc

--- tools/bin/setup_zksolc.sh
+++ tools/bin/setup_zksolc.sh
@@ -4,30 +4,34 @@ set -e
 
 # Specify the version of zksolc you want to install
 VERSION="1.5.6"
-ASSET_NAME="zksolc-linux-amd64-gnu-${VERSION}"
+# ASSET_NAME="zksolc-linux-amd64-gnu-${VERSION}"
+# ASSET_NAME="zksolc-macosx-arm64-v1.5.6"
 
 # # Define the GitHub repository and URL for the release
 REPO="matter-labs/era-compiler-solidity"
 GITHUB_URL="https://api.github.com/repos/$REPO/releases/tags/$VERSION"
 
-ASSET_NAME2="zksolc-linux-amd64-gnu-v${VERSION}"
+# ASSET_NAME2="zksolc-linux-amd64-gnu-v${VERSION}"
+# ASSET_NAME2="zksolc-macosx-arm64-v1.5.6"
+# ASSET_NAME="zksolc-macosx-arm64-v1.5.6"
+ASSET_NAME="zksolc-linux-amd64-gnu-v${VERSION}"
 # Fetch the release info using GitHub API and get the download URL for the asset
-ASSET_URL=$(curl --silent "$GITHUB_URL" | jq -r ".assets[] | select(.name == \"$ASSET_NAME2\") | .browser_download_url")
+ASSET_URL=$(curl --silent "$GITHUB_URL" | jq -r ".assets[] | select(.name == \"$ASSET_NAME\") | .browser_download_url")
 
 if [ -z "$ASSET_URL" ]; then
   echo "Error: Could not find the asset $ASSET_NAME in release $VERSION."
   exit 1
 fi
 
-# # Download the asset
-echo "Downloading $ASSET_NAME... from $ASSET_URL"
-curl -L $ASSET_URL -o zksolc-bin
 
-echo "Creating symbolic link"
-sudo ln -s zksolc-bin /usr/local/bin/zksolc
+# echo "Removing existing link if any"
+# sudo rm /usr/local/bin/zksolc
+
+echo "Downloading $ASSET_NAME... from $ASSET_URL"
+sudo curl -L "$ASSET_URL" -o /usr/local/bin/zksolc
 
 echo "Giving permission to execute"
-chmod +x /usr/local/bin/zksolc
+sudo chmod +x /usr/local/bin/zksolc
 
 echo "Testing"
 zksolc --version
