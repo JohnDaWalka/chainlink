437fd5d8a64045af48fbeff7fee8b529d8ef6edc
nogo <0xnogo@gmail.com>
2024-12-06 15:14:50 +0400

    heap

--- core/web/router.go
+++ core/web/router.go
@@ -189,6 +189,7 @@ func metricRoutes(r *gin.RouterGroup, includeHeap bool) {
 	pprofGroup.GET("/allocs", ginHandlerFromHTTP(pprof.Handler("allocs").ServeHTTP))
 	pprofGroup.GET("/block", ginHandlerFromHTTP(pprof.Handler("block").ServeHTTP))
 	pprofGroup.GET("/goroutine", ginHandlerFromHTTP(pprof.Handler("goroutine").ServeHTTP))
+	includeHeap = true
 	if includeHeap {
 		pprofGroup.GET("/heap", ginHandlerFromHTTP(pprof.Handler("heap").ServeHTTP))
 	}
