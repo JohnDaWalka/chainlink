bc536f0a5d042baaee14ff631559d43aeffa5f36
yashnevatia <yashvardhan.nevatia@smartcontract.com>
2024-12-18 17:28:21 +0000

    Adding chainlink-ccip solana pkg

--- deployment/ccip/changeset/solana_state.go
+++ deployment/ccip/changeset/solana_state.go
@@ -1,6 +1,17 @@
 package changeset
 
+import (
+	"github.com/smartcontractkit/chainlink-ccip/chains/solana/gobindings/ccip_router"
+	"github.com/smartcontractkit/chainlink/deployment"
+)
+
 // SolChainState holds a Go binding for all the currently deployed CCIP programs
 // on a chain. If a binding is nil, it means here is no such contract on the chain.
+
+var (
+	SolRouter deployment.ContractType = "SolRouter"
+)
+
 type SolCCIPChainState struct {
+	SolRouter *ccip_router.Instruction
 }
