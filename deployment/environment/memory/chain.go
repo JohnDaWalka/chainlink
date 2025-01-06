8150d8d41004f220ee25fc32d4dd6c225fc8dcc1
yashnevatia <yashvardhan.nevatia@smartcontract.com>
2025-01-06 13:59:31 +0000

    adding test env setup

--- deployment/environment/memory/chain.go
+++ deployment/environment/memory/chain.go
@@ -15,6 +15,9 @@ import (
 
 	"github.com/smartcontractkit/chainlink-common/pkg/utils/tests"
 
+	"github.com/gagliardetto/solana-go"
+	solRpc "github.com/gagliardetto/solana-go/rpc"
+	solTestUtil "github.com/smartcontractkit/chainlink-ccip/chains/solana/contracts/tests/testutils"
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/assets"
 )
 
@@ -22,6 +25,11 @@ type EVMChain struct {
 	Backend     *simulated.Backend
 	DeployerKey *bind.TransactOpts
 	Users       []*bind.TransactOpts
+}
+
+type SolanaChain struct {
+	Client      *solRpc.Client
+	DeployerKey *solana.PrivateKey
 }
 
 func fundAddress(t *testing.T, from *bind.TransactOpts, to common.Address, amount *big.Int, backend *simulated.Backend) {
@@ -49,6 +57,37 @@ func GenerateChains(t *testing.T, numChains int, numUsers int) map[uint64]EVMCha
 	for i := 0; i < numChains; i++ {
 		chainID := chainsel.TEST_90000001.EvmChainID + uint64(i)
 		chains[chainID] = evmChain(t, numUsers)
+	}
+	return chains
+}
+
+func getTestSolanaChainSelectors() []uint64 {
+	result := []uint64{}
+	for _, x := range chainsel.SolanaALL {
+		if x.Name == x.ChainID {
+			result = append(result, x.Selector)
+		}
+	}
+	return result
+}
+
+func GenerateChainsSol(t *testing.T, numChains int) map[uint64]SolanaChain {
+
+	chains := make(map[uint64]SolanaChain)
+	testSolanaChainSelectors := getTestSolanaChainSelectors()
+	if len(testSolanaChainSelectors) < numChains {
+		t.Fatalf("not enough test solana chain selectors available")
+	}
+
+	for i := 0; i < numChains; i++ {
+		chainID := testSolanaChainSelectors[i]
+		url, _ := solTestUtil.SetupLocalSolNodeWithFlags(t)
+		admin, gerr := solana.NewRandomPrivateKey()
+		require.NoError(t, gerr)
+		chains[chainID] = SolanaChain{
+			Client:      solRpc.New(url),
+			DeployerKey: &admin,
+		}
 	}
 	return chains
 }
