b9cb7e641d3330c09f44ba197552249ee14d5a18
Bartek Tofel <tofel.b@gmail.com>
2025-01-23 17:11:36 +0100

    move the function

--- integration-tests/capabilities/workflow_test.go
+++ integration-tests/capabilities/workflow_test.go
@@ -9,7 +9,6 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"math"
@@ -26,9 +25,12 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/go-yaml/yaml"
+	"github.com/google/go-github/v41/github"
+	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/oauth2"
 
 	"github.com/smartcontractkit/libocr/offchainreporting2/confighelper"
 	"github.com/smartcontractkit/libocr/offchainreporting2/types"
@@ -152,6 +154,75 @@ func extractKey(value string) string {
 		return parts[len(parts)-1]
 	}
 	return value
+}
+
+func downloadGHAssetFromLatestRelease(owner, repository, releaseType, assetName, ghToken string) ([]byte, error) {
+	var content []byte
+	if ghToken == "" {
+		return content, errors.New("no github token provided")
+	}
+
+	if (releaseType == test_env.AUTOMATIC_LATEST_TAG) || (releaseType == test_env.AUTOMATIC_STABLE_LATEST_TAG) {
+		ctx := context.Background()
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: ghToken},
+		)
+		tc := oauth2.NewClient(ctx, ts)
+
+		ghClient := github.NewClient(tc)
+
+		latestTags, _, err := ghClient.Repositories.ListReleases(context.Background(), owner, repository, &github.ListOptions{PerPage: 20})
+		if err != nil {
+			return content, errors.Wrapf(err, "failed to list releases for %s", repository)
+		}
+
+		var latestRelease *github.RepositoryRelease
+		for _, tag := range latestTags {
+			if releaseType == test_env.AUTOMATIC_STABLE_LATEST_TAG {
+				if tag.Prerelease != nil && *tag.Prerelease {
+					continue
+				}
+				if tag.Draft != nil && *tag.Draft {
+					continue
+				}
+			}
+			if tag.TagName != nil {
+				latestRelease = tag
+				break
+			}
+		}
+
+		if latestRelease == nil {
+			return content, errors.New("failed to find latest release with automatic tag: " + releaseType)
+		}
+
+		var assetId int64
+		for _, asset := range latestRelease.Assets {
+			if strings.Contains(asset.GetName(), assetName) {
+				assetId = asset.GetID()
+				break
+			}
+		}
+
+		if assetId == 0 {
+			return content, fmt.Errorf("failed to find asset %s for %s", assetName, *latestRelease.TagName)
+		}
+
+		asset, _, err := ghClient.Repositories.DownloadReleaseAsset(context.Background(), owner, repository, assetId, tc)
+		if err != nil {
+			return content, errors.Wrapf(err, "failed to download asset %s for %s", assetName, *latestRelease.TagName)
+		}
+
+		content, err = io.ReadAll(asset)
+		if err != nil {
+			return content, err
+		}
+
+		return content, nil
+
+	}
+
+	return content, errors.New("no automatic tag provided")
 }
 
 func getNodesInfo(
