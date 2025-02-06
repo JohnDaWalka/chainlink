5236a7d16b59a17e04ae69759a6116f37aa0cd49
andrejrakic <andrej.rakic@smartcontract.com>
2025-02-06 15:32:36 +0100

    chore: Add tests for roles for BurnMintERC20PausableUUPS

---
+++ contracts/src/v0.8/shared/test/token/ERC20/upgradeable/BurnMintERC20PausableUUPS/BurnMintERC20PausableUUPS.roles.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {
+  BurnMintERC20PausableUUPS,
+  IAccessControl
+} from "../../../../../token/ERC20/upgradeable/BurnMintERC20PausableUUPS.sol";
+import {BurnMintERC20PausableUUPSSetup} from "./BurnMintERC20PausableUUPSSetup.t.sol";
+
+contract BurnMintERC20PausableUUPS_roles is BurnMintERC20PausableUUPSSetup {
+  function test_GrantMintAndBurnRoles() public {
+    assertFalse(s_burnMintERC20PausableUUPS.hasRole(s_burnMintERC20PausableUUPS.MINTER_ROLE(), STRANGER));
+    assertFalse(s_burnMintERC20PausableUUPS.hasRole(s_burnMintERC20PausableUUPS.BURNER_ROLE(), STRANGER));
+
+    changePrank(s_defaultAdmin);
+
+    vm.expectEmit();
+    emit IAccessControl.RoleGranted(s_burnMintERC20PausableUUPS.MINTER_ROLE(), STRANGER, OWNER);
+    vm.expectEmit();
+    emit IAccessControl.RoleGranted(s_burnMintERC20PausableUUPS.BURNER_ROLE(), STRANGER, OWNER);
+
+    s_burnMintERC20PausableUUPS.grantMintAndBurnRoles(STRANGER);
+
+    assertTrue(s_burnMintERC20PausableUUPS.hasRole(s_burnMintERC20PausableUUPS.MINTER_ROLE(), STRANGER));
+    assertTrue(s_burnMintERC20PausableUUPS.hasRole(s_burnMintERC20PausableUUPS.BURNER_ROLE(), STRANGER));
+  }
+
+  function test_GetCCIPAdmin() public view {
+    assertEq(s_burnMintERC20PausableUUPS.getCCIPAdmin(), s_defaultAdmin);
+  }
+
+  function test_SetCCIPAdmin() public {
+    changePrank(s_defaultAdmin);
+
+    vm.expectEmit();
+    emit BurnMintERC20PausableUUPS.CCIPAdminTransferred(s_defaultAdmin, STRANGER);
+
+    s_burnMintERC20PausableUUPS.setCCIPAdmin(STRANGER);
+
+    assertEq(s_burnMintERC20PausableUUPS.getCCIPAdmin(), STRANGER);
+  }
+}
