3f06a64f956f533227c3e04433d4849a5d15d633
andrejrakic <andrej.rakic@smartcontract.com>
2025-02-06 15:32:14 +0100

    chore: Add tests for pausing for BurnMintERC20PausableUUPS

---
+++ contracts/src/v0.8/shared/test/token/ERC20/upgradeable/BurnMintERC20PausableUUPS/BurnMintERC20PausableUUPS.pausing.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {BurnMintERC20PausableUUPS, IERC20} from "../../../../../token/ERC20/upgradeable/BurnMintERC20PausableUUPS.sol";
+import {BurnMintERC20PausableUUPSSetup} from "./BurnMintERC20PausableUUPSSetup.t.sol";
+
+contract BurnMintERC20PausableUUPS_pausing is BurnMintERC20PausableUUPSSetup {
+  address s_mockPool;
+  uint256 s_amount;
+
+  function setUp() public virtual override {
+    BurnMintERC20PausableUUPSSetup.setUp();
+
+    s_mockPool = makeAddr("s_mockPool");
+
+    s_burnMintERC20PausableUUPS.grantMintAndBurnRoles(s_mockPool);
+    s_amount = 1e18;
+  }
+
+  function test_Pause() public {
+    changePrank(s_defaultPauser);
+    s_burnMintERC20PausableUUPS.pause();
+
+    changePrank(s_mockPool);
+    vm.expectRevert(abi.encodeWithSelector(BurnMintERC20PausableUUPS.BurnMintERC20PausableUUPS__Paused.selector));
+    s_burnMintERC20PausableUUPS.mint(STRANGER, s_amount);
+  }
+
+  function test_Unpause() public {
+    changePrank(s_defaultPauser);
+    s_burnMintERC20PausableUUPS.pause();
+    s_burnMintERC20PausableUUPS.unpause();
+
+    changePrank(s_mockPool);
+    vm.expectEmit();
+    emit IERC20.Transfer(address(0), STRANGER, s_amount);
+    s_burnMintERC20PausableUUPS.mint(STRANGER, s_amount);
+  }
+}
