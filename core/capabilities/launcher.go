73e6885bb80c30391f27c236f731c28ab93f9c60
jinhoonbang <jin.bang@smartcontract.com>
2025-02-27 10:45:54 -0800

    remove don id hard coding

--- core/capabilities/launcher.go
+++ core/capabilities/launcher.go
@@ -240,10 +240,6 @@ func (w *launcher) Launch(ctx context.Context, state *registrysyncer.LocalRegist
 				return err
 			}
 		}
-	} else { // TODO: get rid of this. This unblocks us from using the workflow syncer.
-		w.workflowDonNotifier.NotifyDonSet(capabilities.DON{
-			ID: 1,
-		})
 	}
 
 	// Finally, if I'm in a capability DON, let's enable external access
