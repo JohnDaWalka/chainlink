dc6ebe9b5ec46787eea5ad8e474f54e3301714d6
Ilan <ilan.olkies@smartcontract.com>
2025-03-12 17:46:45 -0300

    Failing test to get started with

--- deployment/ccip/changeset/cs_prerequisites_test.go
+++ deployment/ccip/changeset/cs_prerequisites_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zapcore"
 
+	"github.com/smartcontractkit/chainlink/deployment"
 	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset"
 	"github.com/smartcontractkit/chainlink/deployment/environment/memory"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
@@ -13,28 +14,51 @@ import (
 
 func TestDeployPrerequisites(t *testing.T) {
 	t.Parallel()
-	lggr := logger.TestLogger(t)
-	e := memory.NewMemoryEnvironment(t, lggr, zapcore.InfoLevel, memory.MemoryEnvironmentConfig{
-		Bootstraps: 1,
-		Chains:     2,
-		Nodes:      4,
-	})
-	newChain := e.AllChainSelectors()[0]
-	cfg := changeset.DeployPrerequisiteConfig{
-		Configs: []changeset.DeployPrerequisiteConfigPerChain{
-			{
-				ChainSelector: newChain,
-			},
+	testCases := []struct {
+		name   string
+		config memory.MemoryEnvironmentConfig
+	}{{
+		name: "TestDeployPrerequisitesEVM",
+		config: memory.MemoryEnvironmentConfig{
+			Bootstraps: 1,
+			Chains:     2,
+			Nodes:      4,
 		},
+	}, { // this is failing now...
+		name: "TestDeployPrerequisitesZKVM",
+		config: memory.MemoryEnvironmentConfig{
+			Bootstraps: 1,
+			Chains:     0,
+			Nodes:      4,
+			ZKChains:   2,
+		},
+	}}
+
+	lggr := logger.TestLogger(t)
+	for _, testCase := range testCases {
+		t.Run(testCase.name, testDeployPrerequisitesForChain(memory.NewMemoryEnvironment(t, lggr, zapcore.InfoLevel, testCase.config)))
 	}
-	output, err := changeset.DeployPrerequisitesChangeset(e, cfg)
-	require.NoError(t, err)
-	err = e.ExistingAddresses.Merge(output.AddressBook)
-	require.NoError(t, err)
-	state, err := changeset.LoadOnchainState(e)
-	require.NoError(t, err)
-	require.NotNil(t, state.Chains[newChain].Weth9)
-	require.NotNil(t, state.Chains[newChain].TokenAdminRegistry)
-	require.NotNil(t, state.Chains[newChain].RegistryModule)
-	require.NotNil(t, state.Chains[newChain].Router)
+}
+
+func testDeployPrerequisitesForChain(e deployment.Environment) func(t *testing.T) {
+	return func(t *testing.T) {
+		newChain := e.AllChainSelectors()[0]
+		cfg := changeset.DeployPrerequisiteConfig{
+			Configs: []changeset.DeployPrerequisiteConfigPerChain{
+				{
+					ChainSelector: newChain,
+				},
+			},
+		}
+		output, err := changeset.DeployPrerequisitesChangeset(e, cfg)
+		require.NoError(t, err)
+		err = e.ExistingAddresses.Merge(output.AddressBook)
+		require.NoError(t, err)
+		state, err := changeset.LoadOnchainState(e)
+		require.NoError(t, err)
+		require.NotNil(t, state.Chains[newChain].Weth9)
+		require.NotNil(t, state.Chains[newChain].TokenAdminRegistry)
+		require.NotNil(t, state.Chains[newChain].RegistryModule)
+		require.NotNil(t, state.Chains[newChain].Router)
+	}
 }
