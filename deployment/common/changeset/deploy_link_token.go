ad5ccfedd0949bb3e0161350e679e16f77b4fb77
Ilan <36084092+ilanolkies@users.noreply.github.com>
2025-03-19 21:50:59 -0300

    Update deploy_link_token.go

--- deployment/common/changeset/deploy_link_token.go
+++ deployment/common/changeset/deploy_link_token.go
@@ -4,12 +4,10 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"golang.org/x/sync/errgroup"
 
-	"github.com/smartcontractkit/chainlink-common/pkg/logger"
-
 	"github.com/gagliardetto/solana-go"
-
 	chainsel "github.com/smartcontractkit/chain-selectors"
 
 	solCommomUtil "github.com/smartcontractkit/chainlink-ccip/chains/solana/utils/common"
