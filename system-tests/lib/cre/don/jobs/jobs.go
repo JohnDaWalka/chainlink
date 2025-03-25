d4ccb0911741e5a7a7f4a9b66e1045ea838f03e6
Bartek Tofel <tofel.b@gmail.com>
2025-03-25 12:56:38 +0100

    use errgroup + rate limit when creating jobs

--- system-tests/lib/cre/don/jobs/jobs.go
+++ system-tests/lib/cre/don/jobs/jobs.go
@@ -2,15 +2,14 @@ package jobs
 
 import (
 	"context"
-	"sync"
 	"time"
 
 	"github.com/pkg/errors"
+	"go.uber.org/ratelimit"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/smartcontractkit/chainlink/deployment"
 	"github.com/smartcontractkit/chainlink/deployment/environment/devenv"
-
-	jobv1 "github.com/smartcontractkit/chainlink-protos/job-distributor/v1/job"
 
 	types "github.com/smartcontractkit/chainlink/system-tests/lib/cre/types"
 )
@@ -20,46 +19,31 @@ func Create(offChainClient deployment.OffchainClient, don *devenv.DON, flags []s
 		return nil
 	}
 
-	errCh := make(chan error, calculateJobCount(jobSpecs))
-	var wg sync.WaitGroup
-	sem := make(chan struct{}, 5)
+	eg := &errgroup.Group{}
+	jobRateLimit := ratelimit.New(5)
 
 	for jobDesc, jobReqs := range jobSpecs {
 		for _, jobReq := range jobReqs {
-			wg.Add(1)
-			sem <- struct{}{}
-			go func(jobReq *jobv1.ProposeJobRequest) {
-				defer wg.Done()
-				defer func() { <-sem }()
+			eg.Go(func() error {
+				jobRateLimit.Take()
 				timeout := time.Second * 60
 				ctx, cancel := context.WithTimeout(context.Background(), timeout)
 				defer cancel()
 				_, err := offChainClient.ProposeJob(ctx, jobReq)
 				if err != nil {
-					errCh <- errors.Wrapf(err, "failed to propose job %s for node %s", jobDesc.Flag, jobReq.NodeId)
+					return errors.Wrapf(err, "failed to propose job %s for node %s", jobDesc.Flag, jobReq.NodeId)
 				}
-				err = ctx.Err()
-				if err != nil {
-					errCh <- errors.Wrapf(err, "timed out after %s proposing job %s for node %s", timeout.String(), jobDesc.Flag, jobReq.NodeId)
+				if ctx.Err() != nil {
+					return errors.Wrapf(err, "timed out after %s proposing job %s for node %s", timeout.String(), jobDesc.Flag, jobReq.NodeId)
 				}
-			}(jobReq)
+
+				return nil
+			})
 		}
 	}
 
-	wg.Wait()
-	close(errCh)
-
-	var finalErr error
-	for err := range errCh {
-		if finalErr == nil {
-			finalErr = err
-		} else {
-			finalErr = errors.Wrap(finalErr, err.Error())
-		}
-	}
-
-	if finalErr != nil {
-		return errors.Wrap(finalErr, "failed to create at least one job for DON")
+	if err := eg.Wait(); err != nil {
+		return errors.Wrap(err, "failed to create at least one job for DON")
 	}
 
 	return nil
