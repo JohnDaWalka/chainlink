6577c3e3e002567b2dfafaee0a733055bd67ed31
Ilan <ilan.olkies@smartcontract.com>
2025-03-27 14:57:49 -0300

    Port Link Token deploy for ZK support

--- deployment/common/changeset/deploy_link_token.go
+++ deployment/common/changeset/deploy_link_token.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"golang.org/x/sync/errgroup"
 
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
+
 	"github.com/gagliardetto/solana-go"
+
 	chainsel "github.com/smartcontractkit/chain-selectors"
 
 	solCommomUtil "github.com/smartcontractkit/chainlink-ccip/chains/solana/utils/common"
@@ -110,9 +112,10 @@ func deployLinkTokenContractEVM(
 ) (*deployment.ContractDeploy[*link_token.LinkToken], error) {
 	linkToken, err := deployment.DeployContract[*link_token.LinkToken](lggr, chain, ab,
 		func(chain deployment.Chain) deployment.ContractDeploy[*link_token.LinkToken] {
-			linkTokenAddr, tx, linkToken, err2 := link_token.DeployLinkToken(
-				chain.DeployerKey,
-				chain.Client,
+			linkTokenAddr, tx, linkToken, err2 := deployment.PickXVMDeployFn(
+				chain,
+				link_token.DeployLinkToken,
+				link_token.DeployLinkTokenZk,
 			)
 			return deployment.ContractDeploy[*link_token.LinkToken]{
 				Address:  linkTokenAddr,
--- deployment/common/changeset/deploy_link_token_test.go
+++ deployment/common/changeset/deploy_link_token_test.go
@@ -3,10 +3,31 @@ package changeset_test
 import (
 	"testing"
 
+	"go.uber.org/zap/zapcore"
+
 	"github.com/smartcontractkit/chainlink/deployment/common/changeset"
+	"github.com/smartcontractkit/chainlink/deployment/environment/memory"
+	"github.com/smartcontractkit/wsrpc/logger"
 )
 
 func TestDeployLinkToken(t *testing.T) {
 	t.Parallel()
-	changeset.DeployLinkTokenTest(t, 0)
+	lggr := logger.Test(t)
+	solChains := 0
+	e := memory.NewMemoryEnvironment(t, lggr, zapcore.InfoLevel, memory.MemoryEnvironmentConfig{
+		Chains:    1,
+		SolChains: solChains,
+	})
+	changeset.DeployLinkTokenTest(t, e, solChains)
+}
+
+func TestDeployLinkTokenZK(t *testing.T) {
+	t.Parallel()
+	lggr := logger.Test(t)
+	e := memory.NewMemoryEnvironment(t, lggr, zapcore.InfoLevel, memory.MemoryEnvironmentConfig{
+		Chains:    0,
+		SolChains: 0,
+		ZkChains:  1,
+	})
+	changeset.DeployLinkTokenTest(t, e, 0)
 }
