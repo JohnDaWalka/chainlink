ab0eaf2ca3c50e7d2f43976f78a5f7ae4c8b4eeb
giogam <151543+giogam@users.noreply.github.com>
2025-04-01 10:43:02 +0200

    chore: 'github.com/stretchr/testify/assert' -> 'github.com/stretchr/testify/require'

--- deployment/datastore/memory_datastore_test.go
+++ deployment/datastore/memory_datastore_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	"github.com/Masterminds/semver/v3"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMemoryDataStore_Merge(t *testing.T) {
@@ -19,17 +19,17 @@ func TestMemoryDataStore_Merge(t *testing.T) {
 		Version:   semver.MustParse("1.0.0"),
 		Qualifier: "qualifier1",
 	})
-	assert.NoError(t, err, "Adding data to dataStore2 should not fail")
+	require.NoError(t, err, "Adding data to dataStore2 should not fail")
 
 	// Merge dataStore2 into dataStore1
 	err = dataStore1.Merge(dataStore2)
-	assert.NoError(t, err, "Merging dataStore2 into dataStore1 should not fail")
+	require.NoError(t, err, "Merging dataStore2 into dataStore1 should not fail")
 
 	// Verify that dataStore1 contains the merged data
 	addressRefs, err := dataStore1.Addresses().Fetch()
-	assert.NoError(t, err, "Fetching addresses from dataStore1 should not fail")
-	assert.Len(t, addressRefs, 1, "dataStore1 should contain 1 address after merge")
-	assert.Equal(t, "0x123", addressRefs[0].Address, "Merged address should match")
+	require.NoError(t, err, "Fetching addresses from dataStore1 should not fail")
+	require.Len(t, addressRefs, 1, "dataStore1 should contain 1 address after merge")
+	require.Equal(t, "0x123", addressRefs[0].Address, "Merged address should match")
 }
 
 func TestSealedMemoryDataStore_Merge(t *testing.T) {
@@ -45,15 +45,15 @@ func TestSealedMemoryDataStore_Merge(t *testing.T) {
 		Version:   semver.MustParse("1.0.0"),
 		Qualifier: "qualifier2",
 	})
-	assert.NoError(t, err, "Adding data to otherDataStore should not fail")
+	require.NoError(t, err, "Adding data to otherDataStore should not fail")
 
 	// Merge otherDataStore into the sealed data store
 	err = sealedDataStore.Merge(otherDataStore.Seal())
-	assert.NoError(t, err, "Merging into a sealed data store should not fail")
+	require.NoError(t, err, "Merging into a sealed data store should not fail")
 
 	// Verify that the sealed data store contains the merged data
 	addressRefs, err := sealedDataStore.Addresses().Fetch()
-	assert.NoError(t, err, "Fetching addresses from sealedDataStore should not fail")
-	assert.Len(t, addressRefs, 1, "sealedDataStore should contain 1 address after merge")
-	assert.Equal(t, "0x456", addressRefs[0].Address, "Merged address should match")
+	require.NoError(t, err, "Fetching addresses from sealedDataStore should not fail")
+	require.Len(t, addressRefs, 1, "sealedDataStore should contain 1 address after merge")
+	require.Equal(t, "0x456", addressRefs[0].Address, "Merged address should match")
 }
