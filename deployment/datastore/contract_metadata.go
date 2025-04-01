d97b49b3d5294ae4c66a3e28d6e5169428de19ff
giogam <151543+giogam@users.noreply.github.com>
2025-04-01 22:42:08 +0200

    feat: adds generic Metadata to ContractMetadata

--- deployment/datastore/contract_metadata.go
+++ deployment/datastore/contract_metadata.go
@@ -5,22 +5,28 @@ import "errors"
 var ErrContractMetadataNotFound = errors.New("contract metadata record not found")
 var ErrContractMetadataExists = errors.New("contract metadata record already exists")
 
-var _ Record[ContractMetadataKey, ContractMetadata] = ContractMetadata{}
+var _ Record[ContractMetadataKey, ContractMetadata[DefaultMetadata]] = ContractMetadata[DefaultMetadata]{}
 
-type ContractMetadata struct {
-	ChainSelector uint64
-	Address       string
-	Metadata      string
+// DefaultMetadata is a default implementation of the custom contract metadata
+// that is used to store the contract metadata in the datastore.
+type DefaultMetadata string
+
+func (d DefaultMetadata) Clone() DefaultMetadata { return d }
+
+type ContractMetadata[M Cloneable[M]] struct {
+	ChainSelector uint64 `json:"chain_selector"`
+	Address       string `json:"address"`
+	Metadata      M      `json:"metadata"`
 }
 
-func (r ContractMetadata) Clone() ContractMetadata {
-	return ContractMetadata{
+func (r ContractMetadata[M]) Clone() ContractMetadata[M] {
+	return ContractMetadata[M]{
 		ChainSelector: r.ChainSelector,
 		Address:       r.Address,
-		Metadata:      r.Metadata,
+		Metadata:      r.Metadata.Clone(),
 	}
 }
 
-func (r ContractMetadata) Key() ContractMetadataKey {
+func (r ContractMetadata[M]) Key() ContractMetadataKey {
 	return NewContractMetadataKey(r.ChainSelector, r.Address)
 }
