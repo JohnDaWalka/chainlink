512c3a4d2cf0cf60f0f21467f26774eac1e8da22
giogam <151543+giogam@users.noreply.github.com>
2025-04-01 23:17:46 +0200

    feat: improve TestMemoryDataStore_Merge

--- deployment/datastore/memory_datastore_test.go
+++ deployment/datastore/memory_datastore_test.go
@@ -8,52 +8,74 @@ import (
 )
 
 func TestMemoryDataStore_Merge(t *testing.T) {
-	// Create two mutable MemoryDataStores
-	dataStore1 := NewMemoryDataStore()
-	dataStore2 := NewMemoryDataStore()
+	tests := []struct {
+		name          string
+		setup         func() (*MemoryDataStore, *MemoryDataStore)
+		expectedCount int
+		expectedLabel string
+	}{
+		{
+			name: "Merge single address",
+			setup: func() (*MemoryDataStore, *MemoryDataStore) {
+				dataStore1 := NewMemoryDataStore()
+				dataStore2 := NewMemoryDataStore()
+				err := dataStore2.Addresses().AddOrUpdate(AddressRef{
+					Address:   "0x123",
+					Type:      "type1",
+					Version:   semver.MustParse("1.0.0"),
+					Qualifier: "qualifier1",
+				})
+				require.NoError(t, err, "Adding data to dataStore2 should not fail")
+				return dataStore1, dataStore2
+			},
+			expectedCount: 1,
+		},
+		{
+			name: "Match existing address with labels",
+			setup: func() (*MemoryDataStore, *MemoryDataStore) {
+				dataStore1 := NewMemoryDataStore()
+				dataStore2 := NewMemoryDataStore()
 
-	// Add some data to the second data store
-	err := dataStore2.Addresses().AddOrUpdate(AddressRef{
-		Address:   "0x123",
-		Type:      "type1",
-		Version:   semver.MustParse("1.0.0"),
-		Qualifier: "qualifier1",
-	})
-	require.NoError(t, err, "Adding data to dataStore2 should not fail")
+				// Add initial data to dataStore1
+				err := dataStore1.Addresses().AddOrUpdate(AddressRef{
+					Address:   "0x123",
+					Type:      "type1",
+					Version:   semver.MustParse("1.0.0"),
+					Qualifier: "qualifier1",
+					Labels:    NewLabelSet("label1"),
+				})
+				require.NoError(t, err, "Adding initial data to dataStore1 should not fail")
 
-	// Merge dataStore2 into dataStore1
-	err = dataStore1.Merge(dataStore2)
-	require.NoError(t, err, "Merging dataStore2 into dataStore1 should not fail")
+				// Add matching data to dataStore2
+				err = dataStore2.Addresses().AddOrUpdate(AddressRef{
+					Address:   "0x123",
+					Type:      "type1",
+					Version:   semver.MustParse("1.0.0"),
+					Qualifier: "qualifier1",
+					Labels:    NewLabelSet("label2"),
+				})
+				require.NoError(t, err, "Adding matching data to dataStore2 should not fail")
 
-	// Verify that dataStore1 contains the merged data
-	addressRefs, err := dataStore1.Addresses().Fetch()
-	require.NoError(t, err, "Fetching addresses from dataStore1 should not fail")
-	require.Len(t, addressRefs, 1, "dataStore1 should contain 1 address after merge")
-	require.Equal(t, "0x123", addressRefs[0].Address, "Merged address should match")
-}
+				return dataStore1, dataStore2
+			},
+			expectedCount: 1,
+			expectedLabel: "label2",
+		},
+	}
 
-func TestSealedMemoryDataStore_Merge(t *testing.T) {
-	// Create a mutable MemoryDataStore and seal it
-	mutableDataStore := NewMemoryDataStore()
-	sealedDataStore := mutableDataStore.Seal()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dataStore1, dataStore2 := tt.setup()
 
-	// Create another mutable MemoryDataStore with some data
-	otherDataStore := NewMemoryDataStore()
-	err := otherDataStore.Addresses().AddOrUpdate(AddressRef{
-		Address:   "0x456",
-		Type:      "type2",
-		Version:   semver.MustParse("1.0.0"),
-		Qualifier: "qualifier2",
-	})
-	require.NoError(t, err, "Adding data to otherDataStore should not fail")
+			// Merge dataStore2 into dataStore1
+			err := dataStore1.Merge(dataStore2.Seal())
+			require.NoError(t, err, "Merging dataStore2 into dataStore1 should not fail")
 
-	// Merge otherDataStore into the sealed data store
-	err = sealedDataStore.Merge(otherDataStore.Seal())
-	require.NoError(t, err, "Merging into a sealed data store should not fail")
-
-	// Verify that the sealed data store contains the merged data
-	addressRefs, err := sealedDataStore.Addresses().Fetch()
-	require.NoError(t, err, "Fetching addresses from sealedDataStore should not fail")
-	require.Len(t, addressRefs, 1, "sealedDataStore should contain 1 address after merge")
-	require.Equal(t, "0x456", addressRefs[0].Address, "Merged address should match")
+			// Verify that dataStore1 contains the merged data
+			addressRefs, err := dataStore1.Addresses().Fetch()
+			require.NoError(t, err, "Fetching addresses from dataStore1 should not fail")
+			require.Len(t, addressRefs, tt.expectedCount, "dataStore1 should contain the expected number of addresses after merge")
+			require.Equal(t, tt.expectedLabel, addressRefs[0].Labels.String(), "Labels should be updated correctly after merge")
+		})
+	}
 }
