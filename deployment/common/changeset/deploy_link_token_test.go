41fcf6778f40dc8dfadfc2b1349c49cf1f823d3a
Ilan <ilan.olkies@smartcontract.com>
2025-04-24 13:03:45 -0300

    Remove unnecesary 0

--- deployment/common/changeset/deploy_link_token_test.go
+++ deployment/common/changeset/deploy_link_token_test.go
@@ -10,17 +10,13 @@ import (
 func TestDeployLinkToken(t *testing.T) {
 	t.Parallel()
 	changeset.DeployLinkTokenTest(t, memory.MemoryEnvironmentConfig{
-		Chains:    1,
-		SolChains: 0,
-		ZkChains:  0,
+		Chains: 1,
 	})
 }
 
 func TestDeployLinkTokenZk(t *testing.T) {
 	t.Parallel()
 	changeset.DeployLinkTokenTest(t, memory.MemoryEnvironmentConfig{
-		Chains:    0,
-		SolChains: 0,
-		ZkChains:  1,
+		ZkChains: 1,
 	})
 }
