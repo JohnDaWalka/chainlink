f973bf6466965472492405cd3549a05fc151bf89
Ilan <ilan.olkies@smartcontract.com>
2025-04-30 15:06:15 -0300

    Remove added line

--- deployment/environment/devenv/chain.go
+++ deployment/environment/devenv/chain.go
@@ -13,7 +13,6 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
-
 	chainselectors "github.com/smartcontractkit/chain-selectors"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
