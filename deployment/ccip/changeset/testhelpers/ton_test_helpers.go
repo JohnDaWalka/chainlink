457c2f999780144d9fed9780c2537e1d7c76e9e9
Brian Sztamfater <brian.sztamfater@smartcontract.com>
2025-04-30 09:44:56 -0300

    some fixes and comment out code on cs ton test helpers and on-memory ton chain

--- deployment/ccip/changeset/testhelpers/ton_test_helpers.go
+++ deployment/ccip/changeset/testhelpers/ton_test_helpers.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/hex"
 	"fmt"
+	"log"
+	"strings"
 	"testing"
 	"time"
 
@@ -11,7 +13,6 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	ethbind "github.com/ethereum/go-ethereum/accounts/abi/bind"
 	chainsel "github.com/smartcontractkit/chain-selectors"
 	"github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
@@ -28,11 +29,11 @@ import (
 	"github.com/smartcontractkit/chainlink-ccip/chains/evm/gobindings/generated/v1_6_0/onramp"
 	"github.com/smartcontractkit/chainlink/deployment"
 	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset"
-	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset/globals"
-	"github.com/smartcontractkit/chainlink/deployment/ccip/changeset/internal"
 	commoncs "github.com/smartcontractkit/chainlink/deployment/common/changeset"
-	"github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/types"
 	tonaddress "github.com/xssnick/tonutils-go/address"
+	"github.com/xssnick/tonutils-go/liteclient"
+	"github.com/xssnick/tonutils-go/ton"
+	"github.com/xssnick/tonutils-go/ton/wallet"
 )
 
 type TonTestDeployPrerequisitesChangeSet struct {
@@ -87,76 +88,65 @@ func (c TonTestDeployContractsChangeSet) Apply(e deployment.Environment) (deploy
 
 func (c TonTestDeployContractsChangeSet) deployTonContracts(t *testing.T, e deployment.Environment, chainSelector uint64, tonChain deployment.TonChain, tonChainState changeset.TonCCIPChainState, onchainState changeset.CCIPOnChainState) {
 	logger := logger.Test(t)
-	adminAddress := tonChain.DeployerSeed.AccountAddress()
 
-	mcmsSeed := fmt.Sprintf("%d", time.Now().UnixNano())
-	mcmsAddress, mcmsPendingTx, mcmsBindings, err := mcms.DeployToResourceAccount(tonChain.DeployerSigner, tonChain.Client, mcmsSeed)
-	require.NoError(t, err)
-	logger.Infow("Deployed Ton MCMS", "address", mcmsAddress.String(), "pendingTx", mcmsPendingTx.TxnHash())
-	_ = mcmsBindings
-	waitForTx(t, tonChain.Client, mcmsPendingTx.TxnHash(), time.Minute*1)
+	connectionPool := liteclient.NewConnectionPool()
 
-	ccipAddress, ccipPendingTx, ccipBindings, err := ccip.DeployToObject(tonChain.DeployerSigner, tonChain.Client, mcmsAddress, false)
-	require.NoError(t, err)
-	logger.Infow("Deployed Ton CCIP", "address", ccipAddress.String(), "pendingTx", ccipPendingTx.TxnHash())
-	_ = ccipBindings
-	waitForTx(t, tonChain.Client, ccipPendingTx.TxnHash(), time.Minute*1)
+	// move this to a ton config module
+	var (
+		NetworkConfigFile = "http://127.0.0.1:8000/localhost.global.config.json"
+		// FaucetWalletSeed  = "viable model canvas decade neck soap turtle asthma bench crouch bicycle grief history envelope valid intact invest like offer urban adjust popular draft coral"
+		// FaucetSubWalletID = 42
+		// FaucetWalletVer   = wallet.V3R2
+	)
 
-	offrampPendingTx, offrampBindings, err := ccip_offramp.DeployToExistingObject(tonChain.DeployerSigner, tonChain.Client, ccipAddress, ccipAddress, mcmsAddress)
-	require.NoError(t, err)
-	logger.Infow("Deployed Ton Offramp", "address", ccipAddress.String(), "pendingTx", ccipPendingTx.TxnHash())
-	_ = offrampBindings
-	waitForTx(t, tonChain.Client, offrampPendingTx.TxnHash(), time.Minute*1)
-
-	onrampPendingTx, onrampBindings, err := ccip_onramp.DeployToExistingObject(tonChain.DeployerSigner, tonChain.Client, ccipAddress, ccipAddress, mcmsAddress)
-	require.NoError(t, err)
-	logger.Infow("Deployed Ton Onramp", "address", ccipAddress.String(), "pendingTx", ccipPendingTx.TxnHash())
-	_ = onrampBindings
-	waitForTx(t, tonChain.Client, onrampPendingTx.TxnHash(), time.Minute*1)
-
-	tonChainState.CCIPAddress = ccipAddress
-
-	ccipRouterPendingTx, ccipRouterBindings, err := ccip_router.DeployToExistingObject(tonChain.DeployerSigner, tonChain.Client, ccipAddress, ccipAddress, mcmsAddress)
-	require.NoError(t, err)
-	logger.Infow("Deployed Ton CCIP Router", "address", ccipAddress.String(), "pendingTx", ccipRouterPendingTx.TxnHash())
-	_ = ccipRouterBindings
-	waitForTx(t, tonChain.Client, ccipRouterPendingTx.TxnHash(), time.Minute*1)
-
-	ccipDummyReceiverAddress, ccipDummyReceiverPendingTx, ccipDummyReceiverBindings, err := ccip_dummy_receiver.DeployToObject(tonChain.DeployerSigner, tonChain.Client, ccipAddress, mcmsAddress)
-	require.NoError(t, err)
-	logger.Infow("Deployed Ton CCIP Dummy Receiver", "address", ccipDummyReceiverAddress.String(), "pendingTx", ccipDummyReceiverPendingTx.TxnHash())
-	_ = ccipDummyReceiverBindings
-	waitForTx(t, tonChain.Client, ccipDummyReceiverPendingTx.TxnHash(), time.Minute*1)
-
-	tonChainState.ReceiverAddress = ccipDummyReceiverAddress
-	//tonChainState.ReceiverAddress = ton.AccountOne
-
-	transactOpts := &bind.TransactOpts{
-		Signer: tonChain.DeployerSigner,
+	// get config
+	cfg, err := liteclient.GetConfigFromUrl(context.Background(), NetworkConfigFile)
+	if err != nil {
+		log.Fatalln("get config err: ", err.Error())
+		return
 	}
 
-	pendingTx, err := onrampBindings.Onramp().Initialize(transactOpts, chainSelector, ton.AccountZero, adminAddress, []uint64{}, []tonaddress.Address{}, []bool{})
-	require.NoError(t, err)
-	logger.Infow("Initialized Onramp", "pendingTx", pendingTx.TxnHash())
-	waitForTx(t, tonChain.Client, pendingTx.TxnHash(), time.Minute*1)
+	// connect to lite servers
+	err = connectionPool.AddConnectionsFromConfig(context.Background(), cfg)
+	if err != nil {
+		log.Fatalln("connection err: ", err.Error())
+		return
+	}
 
-	// TODO: actually figure out where this value comes from
-	//                                 failed to apply changeset at index 1: invalid changeset config: validate plugin info PluginType: CCIPCommit, Chains: [909606746561742123 5548718428018410741 4457093679053095497]: invalid ccip ocr params: invalid execute off-chain config: MessageVisibilityInterval=8h0m0s does not match the permissionlessExecutionThresholdSeconds in dynamic config =0 for chain 4457093679053095497
-	permissionlessExecutionThresholdSecs := uint32(60 * 60 * 8)
-	pendingTx, err = offrampBindings.Offramp().Initialize(transactOpts, chainSelector, permissionlessExecutionThresholdSecs, []uint64{}, []bool{}, []bool{}, [][]byte{})
-	require.NoError(t, err)
-	logger.Infow("Initialized Offramp", "pendingTx", pendingTx.TxnHash())
-	waitForTx(t, tonChain.Client, pendingTx.TxnHash(), time.Minute*1)
+	// api client with full proof checks
+	api := ton.NewAPIClient(connectionPool, ton.ProofCheckPolicyFast).WithRetry()
+	api.SetTrustedBlockFromConfig(cfg)
 
-	pendingTx, err = ccipBindings.FeeQuoter().Initialize(transactOpts, uint64(1000000), tonChainState.LinkTokenAddress, uint64(1000000), []tonaddress.Address{tonChainState.LinkTokenAddress})
-	logger.Infow("Initialized FeeQuoter", "pendingTx", pendingTx.TxnHash())
-	require.NoError(t, err)
-	waitForTx(t, tonChain.Client, pendingTx.TxnHash(), time.Minute*1)
+	_, err = wallet.FromSeed(api, strings.Fields(tonChain.DeployerSeed), wallet.V3R2)
 
-	pendingTx, err = ccipBindings.RMNRemote().Initialize(transactOpts, chainSelector)
-	logger.Infow("Initialized RMNRemote", "pendingTx", pendingTx.TxnHash())
-	require.NoError(t, err)
-	waitForTx(t, tonChain.Client, pendingTx.TxnHash(), time.Minute*1)
+	if err != nil {
+		log.Fatalln("connection err: ", err.Error())
+		return
+	}
+
+	//TODO(ton): Deploy Ton MCMS
+
+	//TODO(ton): Deploy Ton CCIP
+
+	//TODO(ton): Deploy Ton CCIP Offramp
+
+	//TODO(ton): Deploy Ton CCIP Onramp
+
+	//TODO(ton): Deploy Ton CCIP Router
+
+	//TODO(ton): Deploy Ton CCIP Dummy Receiver and set the contract address
+	ccipDummyReceiverAddress := tonaddress.NewAddressNone()
+
+	tonChainState.ReceiverAddress = *ccipDummyReceiverAddress
+	//tonChainState.ReceiverAddress = ton.AccountOne
+
+	//TODO(ton): Initialize Onramp
+
+	//TODO(ton): Initialize Offramp
+
+	//TODO(ton): Initialize FeeQuoter
+
+	//TODO(ton): Initialize RMNRemote
 
 	logger.Infow("All Ton contracts deployed")
 
@@ -191,100 +181,100 @@ func (c TonTestConfigureContractsChangeSet) Apply(e deployment.Environment) (dep
 }
 
 func (c TonTestConfigureContractsChangeSet) configureTonContracts(t *testing.T, e deployment.Environment, chainSelector uint64, tonChain deployment.TonChain, tonChainState changeset.TonCCIPChainState, onchainState changeset.CCIPOnChainState) {
-	logger := logger.Test(t)
+	// logger := logger.Test(t)
 
-	offrampBindings := ccip_offramp.Bind(tonChainState.CCIPAddress, tonChain.Client)
+	// offrampBindings := ccip_offramp.Bind(tonChainState.CCIPAddress, tonChain.Client)
 
-	transactOpts := &bind.TransactOpts{
-		Signer: tonChain.DeployerSigner,
-	}
+	// transactOpts := &bind.TransactOpts{
+	// 	Signer: tonChain.DeployerSigner,
+	// }
 
-	donID, err := internal.DonIDForChain(
-		onchainState.Chains[c.HomeChainSelector].CapabilityRegistry,
-		onchainState.Chains[c.HomeChainSelector].CCIPHome,
-		chainSelector,
-	)
-	require.NoError(t, err)
-	fmt.Printf("Ton DON ID: %+v\n", donID)
+	// donID, err := internal.DonIDForChain(
+	// 	onchainState.Chains[c.HomeChainSelector].CapabilityRegistry,
+	// 	onchainState.Chains[c.HomeChainSelector].CCIPHome,
+	// 	chainSelector,
+	// )
+	// require.NoError(t, err)
+	// fmt.Printf("Ton DON ID: %+v\n", donID)
 
-	allCommitConfigs, err := onchainState.Chains[c.HomeChainSelector].CCIPHome.GetAllConfigs(&ethbind.CallOpts{
-		Context: context.Background(),
-	}, donID, 0)
+	// allCommitConfigs, err := onchainState.Chains[c.HomeChainSelector].CCIPHome.GetAllConfigs(&ethbind.CallOpts{
+	// 	Context: context.Background(),
+	// }, donID, 0)
 
-	allExecConfigs, err := onchainState.Chains[c.HomeChainSelector].CCIPHome.GetAllConfigs(&ethbind.CallOpts{
-		Context: context.Background(),
-	}, donID, 1)
+	// allExecConfigs, err := onchainState.Chains[c.HomeChainSelector].CCIPHome.GetAllConfigs(&ethbind.CallOpts{
+	// 	Context: context.Background(),
+	// }, donID, 1)
 
-	fmt.Printf("DEBUG HOME CHAIN CCIPHome: commit configs: %+v\n", allCommitConfigs)
-	fmt.Printf("DEBUG HOME CHAIN CCIPHome: exec configs: %+v\n", allExecConfigs)
+	// fmt.Printf("DEBUG HOME CHAIN CCIPHome: commit configs: %+v\n", allCommitConfigs)
+	// fmt.Printf("DEBUG HOME CHAIN CCIPHome: exec configs: %+v\n", allExecConfigs)
 
-	ocr3Args, err := internal.BuildSetOCR3ConfigArgsTon(
-		donID, onchainState.Chains[c.HomeChainSelector].CCIPHome, chainSelector, globals.ConfigTypeActive)
-	require.NoError(t, err)
+	// ocr3Args, err := internal.BuildSetOCR3ConfigArgsTon(
+	// 	donID, onchainState.Chains[c.HomeChainSelector].CCIPHome, chainSelector, globals.ConfigTypeActive)
+	// require.NoError(t, err)
 
-	var commitArgs *internal.MultiOCR3BaseOCRConfigArgsTon = nil
-	var execArgs *internal.MultiOCR3BaseOCRConfigArgsTon = nil
-	for _, ocr3Arg := range ocr3Args {
-		if ocr3Arg.OcrPluginType == uint8(types.PluginTypeCCIPCommit) {
-			commitArgs = &ocr3Arg
-		} else if ocr3Arg.OcrPluginType == uint8(types.PluginTypeCCIPExec) {
-			execArgs = &ocr3Arg
-		} else {
-			t.Fatalf("unexpected ocr3 plugin type %s", ocr3Arg.OcrPluginType)
-		}
-	}
-	require.NotNil(t, commitArgs)
-	require.NotNil(t, execArgs)
+	// var commitArgs *internal.MultiOCR3BaseOCRConfigArgsTon = nil
+	// var execArgs *internal.MultiOCR3BaseOCRConfigArgsTon = nil
+	// for _, ocr3Arg := range ocr3Args {
+	// 	if ocr3Arg.OcrPluginType == uint8(types.PluginTypeCCIPCommit) {
+	// 		commitArgs = &ocr3Arg
+	// 	} else if ocr3Arg.OcrPluginType == uint8(types.PluginTypeCCIPExec) {
+	// 		execArgs = &ocr3Arg
+	// 	} else {
+	// 		t.Fatalf("unexpected ocr3 plugin type %s", ocr3Arg.OcrPluginType)
+	// 	}
+	// }
+	// require.NotNil(t, commitArgs)
+	// require.NotNil(t, execArgs)
 
-	commitSigners := [][]byte{}
-	for _, signer := range commitArgs.Signers {
-		commitSigners = append(commitSigners, signer)
-		fmt.Printf("DEBUG: configureTonContracts commit signer %x\n", signer)
-	}
-	commitTransmitters := []tonaddress.Address{}
-	for _, transmitter := range commitArgs.Transmitters {
-		address, err := utils.PublicKeyBytesToAddress(transmitter)
-		require.NoError(t, err)
-		commitTransmitters = append(commitTransmitters, address)
-	}
-	pendingTx, err := offrampBindings.Offramp().SetOcr3Config(transactOpts, commitArgs.ConfigDigest[:], uint8(types.PluginTypeCCIPCommit), commitArgs.F, commitArgs.IsSignatureVerificationEnabled, commitSigners, commitTransmitters)
-	require.NoError(t, err)
-	waitForTx(t, tonChain.Client, pendingTx.TxnHash(), time.Minute*1)
+	// commitSigners := [][]byte{}
+	// for _, signer := range commitArgs.Signers {
+	// 	commitSigners = append(commitSigners, signer)
+	// 	fmt.Printf("DEBUG: configureTonContracts commit signer %x\n", signer)
+	// }
+	// commitTransmitters := []tonaddress.Address{}
+	// for _, transmitter := range commitArgs.Transmitters {
+	// 	address, err := utils.PublicKeyBytesToAddress(transmitter)
+	// 	require.NoError(t, err)
+	// 	commitTransmitters = append(commitTransmitters, address)
+	// }
+	// pendingTx, err := offrampBindings.Offramp().SetOcr3Config(transactOpts, commitArgs.ConfigDigest[:], uint8(types.PluginTypeCCIPCommit), commitArgs.F, commitArgs.IsSignatureVerificationEnabled, commitSigners, commitTransmitters)
+	// require.NoError(t, err)
+	// waitForTx(t, tonChain.Client, pendingTx.TxnHash(), time.Minute*1)
 
-	execSigners := [][]byte{}
-	for _, signer := range execArgs.Signers {
-		execSigners = append(execSigners, signer)
-	}
-	execTransmitters := []tonaddress.Address{}
-	for _, transmitter := range execArgs.Transmitters {
-		address, err := utils.PublicKeyBytesToAddress(transmitter)
-		require.NoError(t, err)
-		execTransmitters = append(execTransmitters, address)
-	}
-	pendingTx, err = offrampBindings.Offramp().SetOcr3Config(transactOpts, execArgs.ConfigDigest[:], uint8(types.PluginTypeCCIPExec), execArgs.F, execArgs.IsSignatureVerificationEnabled, execSigners, execTransmitters)
-	require.NoError(t, err)
-	waitForTx(t, tonChain.Client, pendingTx.TxnHash(), time.Minute*1)
+	// execSigners := [][]byte{}
+	// for _, signer := range execArgs.Signers {
+	// 	execSigners = append(execSigners, signer)
+	// }
+	// execTransmitters := []tonaddress.Address{}
+	// for _, transmitter := range execArgs.Transmitters {
+	// 	address, err := utils.PublicKeyBytesToAddress(transmitter)
+	// 	require.NoError(t, err)
+	// 	execTransmitters = append(execTransmitters, address)
+	// }
+	// pendingTx, err = offrampBindings.Offramp().SetOcr3Config(transactOpts, execArgs.ConfigDigest[:], uint8(types.PluginTypeCCIPExec), execArgs.F, execArgs.IsSignatureVerificationEnabled, execSigners, execTransmitters)
+	// require.NoError(t, err)
+	// waitForTx(t, tonChain.Client, pendingTx.TxnHash(), time.Minute*1)
 
-	logger.Infow("Ton contracts configured")
+	// logger.Infow("Ton contracts configured")
 
-	for _, transmitter := range append(commitTransmitters, execTransmitters...) {
-		// 10 APT
-		entryFunction, err := ton.CoinTransferPayload(nil, transmitter, 1000000000)
-		require.NoError(t, err)
+	// for _, transmitter := range append(commitTransmitters, execTransmitters...) {
+	// 	// 10 TON
+	// 	entryFunction, err := ton.CoinTransferPayload(nil, transmitter, 1000000000)
+	// 	require.NoError(t, err)
 
-		rawTxn, err := tonChain.Client.BuildTransaction(tonChain.DeployerSigner.AccountAddress(), ton.TransactionPayload{Payload: entryFunction})
-		require.NoError(t, err)
+	// 	rawTxn, err := tonChain.Client.BuildTransaction(tonChain.DeployerSigner.AccountAddress(), ton.TransactionPayload{Payload: entryFunction})
+	// 	require.NoError(t, err)
 
-		signedTxn, err := rawTxn.SignedTransaction(tonChain.DeployerSigner)
-		require.NoError(t, err)
+	// 	signedTxn, err := rawTxn.SignedTransaction(tonChain.DeployerSigner)
+	// 	require.NoError(t, err)
 
-		submitResult, err := tonChain.Client.SubmitTransaction(signedTxn)
-		require.NoError(t, err)
+	// 	submitResult, err := tonChain.Client.SubmitTransaction(signedTxn)
+	// 	require.NoError(t, err)
 
-		waitForTx(t, tonChain.Client, submitResult.Hash, time.Minute*1)
+	// 	waitForTx(t, tonChain.Client, submitResult.Hash, time.Minute*1)
 
-		fmt.Printf("Sent 10 APT to transmitter %s\n", transmitter.String())
-	}
+	// 	fmt.Printf("Sent 10 TON to transmitter %s\n", transmitter.String())
+	// }
 }
 
 func addLaneTonChangesets(t *testing.T, e *DeployedEnv, from, to uint64, fromFamily, toFamily string) []commoncs.ConfiguredChangeSet {
@@ -391,10 +381,10 @@ func ConfirmCommitWithExpectedSeqNumRangeTon(
 	return nil, errors.New("TODO(ton): ConfirmCommitWithExpectedSeqNumRangeTon")
 }
 
-func waitForTx(t *testing.T, client ton.TonRpcClient, txHash string, duration time.Duration) {
-	userTx, err := client.WaitForTransaction(txHash, ton.PollTimeout(duration))
-	require.NoError(t, err)
-	require.True(t, userTx.Success, "transaction failed: %s", userTx.VmStatus)
+func waitForTx(t *testing.T, client *ton.APIClient, txHash string, duration time.Duration) {
+	// userTx, err := client.WaitForTransaction(txHash, ton.PollTimeout(duration))
+	// require.NoError(t, err)
+	// require.True(t, userTx.Success, "transaction failed: %s", userTx.VmStatus)
 }
 
 func ConfirmExecWithSeqNrsTon(
