2fae09e8f60fa33f616baaa24cf95c76a761986c
ajaskolski <adrian.jaskolski@smartcontract.com>
2025-05-05 09:16:21 +0200

    fix(deployment):migrate changeset by aliases usage to cldf

---
+++ deployment/migrated_changeset.go
@@ -0,0 +1,40 @@
+package deployment
+
+import (
+	"github.com/smartcontractkit/chainlink-deployments-framework/deployment"
+)
+
+// ChangeSets
+
+type (
+	ChangeSet[C any]            = deployment.ChangeSet[C]
+	ChangeLogic[C any]          = deployment.ChangeLogic[C]
+	PreconditionVerifier[C any] = deployment.PreconditionVerifier[C]
+	ChangeSetV2[C any]          = deployment.ChangeSetV2[C]
+)
+
+var (
+	ErrInvalidConfig      = deployment.ErrInvalidConfig
+	ErrInvalidEnvironment = deployment.ErrInvalidEnvironment
+
+	CreateChangeSet       = deployment.CreateChangeSet
+	CreateLegacyChangeSet = deployment.CreateLegacyChangeSet
+)
+
+// Changeset Output
+
+type (
+	ChangesetOutput = deployment.ChangesetOutput
+	ProposedJob     = deployment.ProposedJob
+)
+
+var (
+	MergeChangesetOutput = deployment.MergeChangesetOutput
+)
+
+// ViewState
+
+type (
+	ViewState   = deployment.ViewState
+	ViewStateV2 = deployment.ViewStateV2
+)
