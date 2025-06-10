28e52f98069e9c40f534f7563f11607d5bff4846
Geert G <117188496+cll-gg@users.noreply.github.com>
2025-06-10 12:32:09 +0100

    Fixes after merging with develop

--- core/services/workflows/v2/disallowed_capability_executor.go
+++ core/services/workflows/v2/disallowed_capability_executor.go
@@ -3,6 +3,7 @@ package v2
 import (
 	"context"
 	"errors"
+	"time"
 
 	sdkpb "github.com/smartcontractkit/chainlink-common/pkg/workflows/sdk/v2/pb"
 	"github.com/smartcontractkit/chainlink-common/pkg/workflows/wasm/host"
@@ -10,8 +11,20 @@ import (
 
 type DisallowedCapabilityExecutor struct{}
 
-var _ host.CapabilityExecutor = DisallowedCapabilityExecutor{}
+var _ host.ExecutionHelper = DisallowedCapabilityExecutor{}
 
 func (d DisallowedCapabilityExecutor) CallCapability(_ context.Context, _ *sdkpb.CapabilityRequest) (*sdkpb.CapabilityResponse, error) {
 	return nil, errors.New("capability calls cannot be made during this execution")
+}
+
+func (d DisallowedCapabilityExecutor) GetDONTime() time.Time {
+	return time.Now()
+}
+
+func (d DisallowedCapabilityExecutor) GetId() string {
+	return ""
+}
+
+func (d DisallowedCapabilityExecutor) GetNodeTime() time.Time {
+	return time.Now()
 }
