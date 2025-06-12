537915260ddce31b689dbcf5adaae466440a08b7
vyzaldysanchez <vyzaldysanchez@gmail.com>
2025-06-12 14:32:11 -0400

    Encapsulates `StartBlockchains` logic

--- system-tests/lib/cre/environment/environment.go
+++ system-tests/lib/cre/environment/environment.go
@@ -24,8 +24,6 @@ import (
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink-evm/pkg/logpoller"
 
-	chainselectors "github.com/smartcontractkit/chain-selectors"
-
 	jobv1 "github.com/smartcontractkit/chainlink-protos/job-distributor/v1/job"
 
 	"github.com/smartcontractkit/chainlink-deployments-framework/chain"
@@ -37,7 +35,6 @@ import (
 	"github.com/smartcontractkit/chainlink-testing-framework/framework/components/postgres"
 	ctfconfig "github.com/smartcontractkit/chainlink-testing-framework/lib/config"
 	"github.com/smartcontractkit/chainlink-testing-framework/lib/utils/ptr"
-	"github.com/smartcontractkit/chainlink/deployment/environment/devenv"
 	keystone_changeset "github.com/smartcontractkit/chainlink/deployment/keystone/changeset"
 	workflow_registry_changeset "github.com/smartcontractkit/chainlink/deployment/keystone/changeset/workflowregistry"
 
@@ -57,7 +54,6 @@ import (
 	cretypes "github.com/smartcontractkit/chainlink/system-tests/lib/cre/types"
 	libformat "github.com/smartcontractkit/chainlink/system-tests/lib/format"
 	libfunding "github.com/smartcontractkit/chainlink/system-tests/lib/funding"
-	libinfra "github.com/smartcontractkit/chainlink/system-tests/lib/infra"
 	libnix "github.com/smartcontractkit/chainlink/system-tests/lib/nix"
 	libtypes "github.com/smartcontractkit/chainlink/system-tests/lib/types"
 )
@@ -138,40 +134,17 @@ func SetupTestEnvironment(
 	startTime := time.Now()
 	fmt.Print(libformat.PurpleText("\n[Stage 1/8] Starting %d blockchain(s)\n\n", len(bi.blockchainsInput)))
 
-	blockchainsOutput, bcOutErr := CreateBlockchains(testLogger, bi)
+	startBlockchainsOutput, bcOutErr := StartBlockchains(BlockchainLoggers{
+		lggr:       testLogger,
+		singleFile: singeFileLogger,
+	}, bi)
 	if bcOutErr != nil {
-		return nil, pkgerrors.Wrap(bcOutErr, "failed to create blockchains")
+		return nil, pkgerrors.Wrap(bcOutErr, "failed to start blockchains")
 	}
 
+	blockchainsOutput := startBlockchainsOutput.BlockChainOutputs
 	homeChainOutput := blockchainsOutput[0]
-	chainsConfigs := []devenv.ChainConfig{}
-
-	for _, bcOut := range blockchainsOutput {
-		chainsConfigs = append(chainsConfigs, devenv.ChainConfig{
-			ChainID:   strconv.FormatUint(bcOut.SethClient.Cfg.Network.ChainID, 10),
-			ChainName: bcOut.SethClient.Cfg.Network.Name,
-			ChainType: strings.ToUpper(bcOut.BlockchainOutput.Family),
-			WSRPCs: []devenv.CribRPCs{{
-				External: bcOut.BlockchainOutput.Nodes[0].ExternalWSUrl,
-				Internal: bcOut.BlockchainOutput.Nodes[0].InternalWSUrl,
-			}},
-			HTTPRPCs: []devenv.CribRPCs{{
-				External: bcOut.BlockchainOutput.Nodes[0].ExternalHTTPUrl,
-				Internal: bcOut.BlockchainOutput.Nodes[0].InternalHTTPUrl,
-			}},
-			DeployerKey: bcOut.SethClient.NewTXOpts(seth.WithNonce(nil)), // set nonce to nil, so that it will be fetched from the RPC node
-		})
-	}
-
-	allChains, _, allChainsErr := devenv.NewChains(singeFileLogger, chainsConfigs)
-	if allChainsErr != nil {
-		return nil, pkgerrors.Wrap(allChainsErr, "failed to create chains")
-	}
-
-	blockChains := map[uint64]chain.BlockChain{}
-	for selector, ch := range allChains {
-		blockChains[selector] = ch
-	}
+	blockChains := startBlockchainsOutput.BlockChains
 
 	allChainsCLDEnvironment := &cldf.Environment{
 		Logger:            singeFileLogger,
@@ -726,97 +699,6 @@ func SetupTestEnvironment(
 		NodeOutput:                          nodeSetOutput,
 		CldEnvironment:                      fullCldOutput.Environment,
 	}, nil
-}
-
-type BlockchainsInput struct {
-	blockchainsInput []*blockchain.Input
-	infra            *libtypes.InfraInput
-	nixShell         *libnix.Shell
-}
-
-type BlockchainOutput struct {
-	ChainSelector      uint64
-	ChainID            uint64
-	BlockchainOutput   *blockchain.Output
-	SethClient         *seth.Client
-	DeployerPrivateKey string
-
-	// private data depending crib vs docker
-	c *blockchain.Output // non-nil if running in docker
-}
-
-func CreateBlockchains(
-	testLogger zerolog.Logger,
-	input BlockchainsInput,
-) ([]*BlockchainOutput, error) {
-	if len(input.blockchainsInput) == 0 {
-		return nil, pkgerrors.New("blockchain input is nil")
-	}
-
-	blockchainOutput := make([]*BlockchainOutput, 0)
-	for _, bi := range input.blockchainsInput {
-		var bcOut *blockchain.Output
-		var bcErr error
-		if input.infra.InfraType == libtypes.CRIB {
-			if input.nixShell == nil {
-				return nil, pkgerrors.New("nix shell is nil")
-			}
-
-			deployCribBlockchainInput := &cretypes.DeployCribBlockchainInput{
-				BlockchainInput: bi,
-				NixShell:        input.nixShell,
-				CribConfigsDir:  cribConfigsDir,
-			}
-			bcOut, bcErr = crib.DeployBlockchain(deployCribBlockchainInput)
-			if bcErr != nil {
-				return nil, pkgerrors.Wrap(bcErr, "failed to deploy blockchain")
-			}
-			err := libinfra.WaitForRPCEndpoint(testLogger, bcOut.Nodes[0].ExternalHTTPUrl, 10*time.Minute)
-			if err != nil {
-				return nil, pkgerrors.Wrap(err, "RPC endpoint is not available")
-			}
-		} else {
-			bcOut, bcErr = blockchain.NewBlockchainNetwork(bi)
-			if bcErr != nil {
-				return nil, pkgerrors.Wrap(bcErr, "failed to deploy blockchain")
-			}
-		}
-
-		pkey := os.Getenv("PRIVATE_KEY")
-		if pkey == "" {
-			return nil, pkgerrors.New("PRIVATE_KEY env var must be set")
-		}
-
-		sethClient, err := seth.NewClientBuilder().
-			WithRpcUrl(bcOut.Nodes[0].ExternalWSUrl).
-			WithPrivateKeys([]string{pkey}).
-			// do not check if there's a pending nonce nor check node's health
-			WithProtections(false, false, seth.MustMakeDuration(time.Second)).
-			Build()
-		if err != nil {
-			return nil, pkgerrors.Wrap(err, "failed to create seth client")
-		}
-
-		chainSelector, err := chainselectors.SelectorFromChainId(sethClient.Cfg.Network.ChainID)
-		if err != nil {
-			return nil, pkgerrors.Wrapf(err, "failed to get chain selector for chain id %d", sethClient.Cfg.Network.ChainID)
-		}
-		chainID, err := strconv.ParseUint(bcOut.ChainID, 10, 64)
-		if err != nil {
-			return nil, pkgerrors.Wrapf(err, "failed to parse chain id %s", bcOut.ChainID)
-		}
-
-		blockchainOutput = append(blockchainOutput, &BlockchainOutput{
-			ChainSelector:      chainSelector,
-			ChainID:            chainID,
-			BlockchainOutput:   bcOut,
-			SethClient:         sethClient,
-			DeployerPrivateKey: pkey,
-			c:                  bcOut,
-		})
-	}
-
-	return blockchainOutput, nil
 }
 
 func CreateJobDistributor(input *jd.Input) (*jd.Output, error) {
