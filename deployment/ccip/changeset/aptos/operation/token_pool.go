48fb2aaebe633b20993438590ce5a387b928c9ef
Rodrigo Soares <38868277+rodrigombsoares@users.noreply.github.com>
2025-06-18 08:48:24 -0300

    fix(deployment): fix some nits in Aptos changesets (#18125)
    * fix(aptos): ApplyPremiumMultiplierWeiPerEthUpdates on chain deployment
    
    * fix nit comments
    
    * fix(aptos): fix nits on aptos changesets
    
    * add min token op
    
    * pad token address | relax timelock mindelay validation
    
    * feat: mint tokens during deployment
    
    * fix: rename tokenObjAddress to tokenCodeObjAddress
    
    * feat: token price updates on lane deployment
    
    * fix lint

--- deployment/ccip/changeset/aptos/operation/token_pool.go
+++ deployment/ccip/changeset/aptos/operation/token_pool.go
@@ -59,7 +59,7 @@ func deployTokenPoolPackage(b operations.Bundle, deps AptosDeps, poolSeed string
 
 type DeployTokenPoolModuleInput struct {
 	PoolType             cldf.ContractType
-	TokenObjAddress      aptos.AccountAddress // TODO change this to metadata address, and determine object if needed
+	TokenCodeObjAddress  aptos.AccountAddress // TODO change this to metadata address, and determine object if needed
 	TokenPoolObjAddress  aptos.AccountAddress
 	InitialAdministrator aptos.AccountAddress
 }
@@ -89,7 +89,7 @@ func deployTokenPoolModule(b operations.Bundle, deps AptosDeps, in DeployTokenPo
 			aptosState.CCIPAddress,
 			aptosState.MCMSAddress,
 			in.TokenPoolObjAddress,
-			in.TokenObjAddress,
+			in.TokenCodeObjAddress,
 			in.InitialAdministrator,
 			true,
 		)
@@ -99,7 +99,7 @@ func deployTokenPoolModule(b operations.Bundle, deps AptosDeps, in DeployTokenPo
 			aptosState.CCIPAddress,
 			aptosState.MCMSAddress,
 			in.TokenPoolObjAddress,
-			in.TokenObjAddress, // TODO this should be the metadata address
+			in.TokenCodeObjAddress, // TODO this should be the metadata address
 			in.InitialAdministrator,
 			true,
 		)
@@ -109,7 +109,7 @@ func deployTokenPoolModule(b operations.Bundle, deps AptosDeps, in DeployTokenPo
 			aptosState.CCIPAddress,
 			aptosState.MCMSAddress,
 			in.TokenPoolObjAddress,
-			in.TokenObjAddress, // TODO this should be the metadata address
+			in.TokenCodeObjAddress, // TODO this should be the metadata address
 			in.InitialAdministrator,
 			true,
 		)
@@ -136,19 +136,19 @@ var GrantMinterPermissionsOp = operations.NewOperation(
 )
 
 type GrantRolePermissionsInput struct {
-	TokenObjAddress       aptos.AccountAddress
+	TokenCodeObjAddress   aptos.AccountAddress
 	TokenPoolStateAddress aptos.AccountAddress
 }
 
 func grantMinterPermissions(b operations.Bundle, deps AptosDeps, in GrantRolePermissionsInput) (types.Transaction, error) {
-	tokenContract := managed_token.Bind(in.TokenObjAddress, deps.AptosChain.Client)
+	tokenContract := managed_token.Bind(in.TokenCodeObjAddress, deps.AptosChain.Client)
 
 	moduleInfo, function, _, args, err := tokenContract.ManagedToken().Encoder().ApplyAllowedMinterUpdates([]aptos.AccountAddress{}, []aptos.AccountAddress{in.TokenPoolStateAddress})
 	if err != nil {
 		return types.Transaction{}, fmt.Errorf("failed to encode ApplyAllowedMinterUpdates: %w", err)
 	}
 
-	return utils.GenerateMCMSTx(in.TokenObjAddress, moduleInfo, function, args)
+	return utils.GenerateMCMSTx(in.TokenCodeObjAddress, moduleInfo, function, args)
 }
 
 // GrantBurnerPermissionsOp operation to grant burner permissions
@@ -160,14 +160,14 @@ var GrantBurnerPermissionsOp = operations.NewOperation(
 )
 
 func grantBurnerPermissions(b operations.Bundle, deps AptosDeps, in GrantRolePermissionsInput) (types.Transaction, error) {
-	tokenContract := managed_token.Bind(in.TokenObjAddress, deps.AptosChain.Client)
+	tokenContract := managed_token.Bind(in.TokenCodeObjAddress, deps.AptosChain.Client)
 
 	moduleInfo, function, _, args, err := tokenContract.ManagedToken().Encoder().ApplyAllowedBurnerUpdates([]aptos.AccountAddress{}, []aptos.AccountAddress{in.TokenPoolStateAddress})
 	if err != nil {
 		return types.Transaction{}, fmt.Errorf("failed to encode ApplyAllowedBurnerUpdates: %w", err)
 	}
 
-	return utils.GenerateMCMSTx(in.TokenObjAddress, moduleInfo, function, args)
+	return utils.GenerateMCMSTx(in.TokenCodeObjAddress, moduleInfo, function, args)
 }
 
 type ApplyChainUpdatesInput struct {
