ca7a8a11241cd723592485b113f3069b7d939d07
Will Winder <wwinder.unh@gmail.com>
2025-06-30 21:47:46 -0400

    Use the new function.

--- core/capabilities/ccip/ccipsolana/msghasher.go
+++ core/capabilities/ccip/ccipsolana/msghasher.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/smartcontractkit/chainlink-ccip/chains/solana/gobindings/ccip_offramp"
 	"github.com/smartcontractkit/chainlink-ccip/chains/solana/utils/ccip"
-	"github.com/smartcontractkit/chainlink-ccip/chains/solana/utils/tokens"
 	cciptypes "github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink/v2/core/capabilities/ccip/common"
@@ -91,7 +90,7 @@ func (h *MessageHasherV1) Hash(_ context.Context, msg cciptypes.Message) (ccipty
 			DestTokenAddress:  solana.PublicKeyFromBytes(ta.DestTokenAddress),
 			ExtraData:         ta.ExtraData,
 			DestGasAmount:     destGasAmount,
-			Amount:            ccip_offramp.CrossChainAmount{LeBytes: tokens.ToLittleEndianU256(ta.Amount.Int.Uint64())},
+			Amount:            ccip_offramp.CrossChainAmount{LeBytes: toLittleEndian(ta.Amount.Int)},
 		})
 	}
 
