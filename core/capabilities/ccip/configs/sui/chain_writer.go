ed8a52fb15156d3fa121d26f3f419aad7376a2f0
stackman27 <sishir.giri@smartcontract.com>
2025-08-05 19:24:01 -0700

    new build

--- core/capabilities/ccip/configs/sui/chain_writer.go
+++ core/capabilities/ccip/configs/sui/chain_writer.go
@@ -8,11 +8,6 @@ import (
 )
 
 func GetChainWriterConfig(publicKeyStr string) (chainwriter.ChainWriterConfig, error) {
-	// fromAddress, err := utils.HexPublicKeyToAddress(publicKeyStr)
-	// if err != nil {
-	// 	return chainwriter.ChainWriterConfig{}, fmt.Errorf("failed to parse Sui address from public key %s: %w", publicKeyStr, err)
-	// }
-
 	isClockMutable := false
 
 	return chainwriter.ChainWriterConfig{
@@ -23,7 +18,6 @@ func GetChainWriterConfig(publicKeyStr string) (chainwriter.ChainWriterConfig, e
 					consts.MethodCommit: {
 						Name:      "commit",
 						PublicKey: []byte(publicKeyStr),
-						// FromAddress: fromAddress.String(),
 						Params: []codec.SuiFunctionParam{
 							{
 								Name:     "object_ref_id",
@@ -61,7 +55,6 @@ func GetChainWriterConfig(publicKeyStr string) (chainwriter.ChainWriterConfig, e
 					consts.MethodExecute: {
 						Name:      "execute",
 						PublicKey: []byte(publicKeyStr),
-						// FromAddress: fromAddress.String(),
 						Params: []codec.SuiFunctionParam{
 							{
 								Name:     "ReportContext",
@@ -82,69 +75,4 @@ func GetChainWriterConfig(publicKeyStr string) (chainwriter.ChainWriterConfig, e
 		// FeeStrategy: chainwriter.DefaultFeeStrategy,
 	}, nil
 
-	// return map[string]any{
-	// 	"modules": map[string]any{
-	// 		consts.ContractNameOffRamp: map[string]any{
-	// 			"name": "offramp",
-	// 			"functions": map[string]any{
-	// 				consts.MethodCommit: map[string]any{
-	// 					"name":         "commit",
-	// 					"public_key":   publicKeyStr,
-	// 					"from_address": fromAddress.String(),
-	// 					"params": []map[string]any{
-	// 						{
-	// 							"name":     "object_ref_id",
-	// 							"type":     "object_id",
-	// 							"required": true,
-	// 						},
-	// 						{
-	// 							"name":      "off_ramp_state_id",
-	// 							"type":      "object_id",
-	// 							"required":  true,
-	// 							"isMutable": false,
-	// 						},
-	// 						{
-	// 							"name":     "clock",
-	// 							"type":     "object_id",
-	// 							"required": true,
-	// 						},
-	// 						{
-	// 							"name":     "ReportContext",
-	// 							"type":     "vector<vector<u8>>",
-	// 							"required": true,
-	// 						},
-	// 						{
-	// 							"name":     "Report",
-	// 							"type":     "vector<u8>",
-	// 							"required": true,
-	// 						},
-	// 						{
-	// 							"name":     "Signatures",
-	// 							"type":     "vector<vector<u8>>",
-	// 							"required": true,
-	// 						},
-	// 					},
-	// 				},
-	// 				consts.MethodExecute: map[string]any{
-	// 					"name":         "execute",
-	// 					"public_key":   publicKeyStr,
-	// 					"from_address": fromAddress.String(),
-	// 					"params": []map[string]any{
-	// 						{
-	// 							"name":     "ReportContext",
-	// 							"type":     "vector<vector<u8>>",
-	// 							"required": true,
-	// 						},
-	// 						{
-	// 							"name":     "Report",
-	// 							"type":     "vector<u8>",
-	// 							"required": true,
-	// 						},
-	// 					},
-	// 				},
-	// 			},
-	// 		},
-	// 	},
-	// 	"fee_strategy": "default", // Assuming chainwriter.DefaultFeeStrategy is a string constant
-	// }, nil
 }
