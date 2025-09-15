7832b9a58d8ce4979aa767fc8fd2eb6d7d27783e
Vladimir Shchukin <vladimir.shchukin@smartcontract.com>
2025-09-15 00:21:36 -0400

    fix linter

--- system-tests/tests/load/cre/workflow_don_load_test.go
+++ system-tests/tests/load/cre/workflow_don_load_test.go
@@ -349,7 +349,7 @@ func TestLoad_Workflow_Streams_MockCapabilities(t *testing.T) {
 	for _, don := range setupOutput.donTopology.DonsWithMetadata {
 		if flags.HasFlag(don.Flags, cretypes.MockCapability) {
 			for _, n := range don.DON.Nodes {
-				key, err2 := n.ExportOCR2Keys(n.Ocr2KeyBundleID)
+				key, err2 := n.ExportOCR2Keys(n.ChainsOcr2KeyBundlesID["evm"])
 				if err2 == nil {
 					b, err3 := json.Marshal(key)
 					require.NoError(t, err3, "could not marshal OCR2 key")
