1b1bde8631306366a3617b4bbda0ec155dd68503
Joe Huang <joe.huang@smartcontract.com>
2025-10-20 06:08:56 -0500

    bump version

--- deployment/ccip/changeset/testhelpers/test_environment.go
+++ deployment/ccip/changeset/testhelpers/test_environment.go
@@ -1164,8 +1164,7 @@ func AddCCIPContractsToEnvironment(t *testing.T, allChains []uint64, tEnv TestEn
 		_, err := memory.GetTONSha()
 		require.NoError(t, err, "failed to get TON commit sha")
 		// TODO replace the hardcoded commit sha with the one fetched from memory.GetTONSha()
-		contractVersion, err := tonUtils.GetContractBuildSHA()
-		require.NoError(t, err, "failed to get TON contract build sha")
+		contractVersion := tonUtils.GetContractBuildSHA()
 		// Allow overriding with a custom version, it's set to "local" on chainlink-ton CI
 		if version := os.Getenv("CCIP_CONTRACTS_TON_VERSION"); version != "" {
 			contractVersion = version
